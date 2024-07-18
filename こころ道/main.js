748c973beb0a8b2f03e908e63d6f27f8f741ac74
yakkoworks <93868999+yakkoworks@users.noreply.github.com>
2024-07-19 03:32:54 +0900

    ulを使う必要はなかった

--- こころ道/main.js
+++ こころ道/main.js
@@ -1,13 +1,23 @@
 function init(){
-    // 職業が増えたらここの数字を忘れずにいじろう
-    var max = 6;
-    for(var i = 1;i<=max;i++){
-        fetch("./0"+i+".html") //ロード元URL
-        .then(data => data.text())
-        .then(html => document.getElementById("content").innerHTML += html)
-    }
+    fetch("./01.html") //ロード元URL
+    .then(data => data.text())
+    .then(html => document.getElementById("content").innerHTML = html)
+    var nav = document.getElementById("nav-01");
+    nav.classList.add("active");
 }
 
+function jump(index){
+    fetch("./"+index+".html") //ロード元URL
+    .then(data => data.text())
+    .then(html => document.getElementById("content").innerHTML = html)
+
+    var navs = document.getElementById("nav").children;
+    for(var i = 0;i<navs.length;i++){
+        navs[i].classList.remove("active");
+    }
+    var nav = document.getElementById("nav-"+index);
+    nav.classList.add("active");
+}
 // function selectJob(){
 // 		var selected = document.getElementById("select_job").value;
 //         fetch("./"+selected+".html") //ロード元URL
