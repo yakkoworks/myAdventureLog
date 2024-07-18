ffce2c88cedff5a31c0feb03db8723ab79457cb9
yakkoworks <93868999+yakkoworks@users.noreply.github.com>
2024-07-19 02:51:58 +0900

    selectタグが使えない問題解決

--- こころ道/main.js
+++ こころ道/main.js
@@ -1,25 +1,16 @@
-// function selectJob(){
-// 	var selected = document.getElementById("select_job").value;
-// 	var iframe = document.getElementById("content");
-// 	iframe.src = "./"+selected+".html";
-// 	// alert(selected);
-// }
-function saveImage(){
-	var node = document.getElementById("content");
-	domtoimage.toPng(node)
-	.then(function (dataUrl) {
-		var link = document.createElement('a');
-        link.download = 'my-image-name.png';
-        link.href = dataUrl;
-        link.click();
-    })
-    .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-    });
-}
-function selectJob(){
-		var selected = document.getElementById("select_job").value;
-        fetch("./"+selected+".html") //ロード元URL
+function init(){
+    // 職業が増えたらここの数字を忘れずにいじろう
+    var max = 6;
+    for(var i = 1;i<=max;i++){
+        fetch("./0"+i+".html") //ロード元URL
         .then(data => data.text())
-        .then(html => document.getElementById("content").innerHTML = html) //ロード先ID指定
+        .then(html => document.getElementById("content").innerHTML += html)
+    }
 }
+
+// function selectJob(){
+// 		var selected = document.getElementById("select_job").value;
+//         fetch("./"+selected+".html") //ロード元URL
+//         .then(data => data.text())
+//         .then(html => document.getElementById("content").innerHTML += html)
+// }
